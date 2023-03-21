04e771f708e5cb68abf48a875f875f3c3eccd7c5
kaiesmahmud <kaiesmahmud@gmail.com>
2023-03-21 10:48:22 +0600

    navbar,hero,about section completed

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,7 +1,15 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+import styles from '../styles';
+import { staggerContainer } from '../utils/motion';
+import { exploreWorlds } from '@/constants';
 const Explore = () => (
-  <section>
+  <section
+    className={`${styles.paddings}`}
+    id="explore"
+  >
     Explore
   </section>
 );
