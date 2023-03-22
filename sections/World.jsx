9f85b9dc8b833a8ec730ee74a247310169ed8898
kaiesmahmud <kaiesmahmud@gmail.com>
2023-03-22 12:52:14 +0600

    insight component updated

--- sections/World.jsx
+++ sections/World.jsx
@@ -2,8 +2,8 @@
 
 import { motion } from 'framer-motion';
 import styles from '../styles';
-import { staggerContainer, fadeIn} from '../utils/motion';
-import {  TitleText, TypingText } from '@/components';
+import { staggerContainer, fadeIn } from '../utils/motion';
+import { TitleText, TypingText } from '@/components';
 
 const World = () => (
   <section
