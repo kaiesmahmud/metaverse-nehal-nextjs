adb291f16788fc3f8cb9ccc5d419801f00d577f1
kaiesmahmud <kaiesmahmud@gmail.com>
2023-03-22 19:58:09 +0600

    bug fixed

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -2,9 +2,9 @@
 
 import { motion } from 'framer-motion';
 import styles from '../styles';
-import { staggerContainer, fadeIn, planetVariants} from '../utils/motion';
-import { NewFeatures, TitleText, TypingText } from '@/components';
-import { newFeatures } from '@/constants';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { NewFeatures, TitleText, TypingText } from '../components';
+import { newFeatures } from '../constants';
 
 const WhatsNew = () => (
   <section
